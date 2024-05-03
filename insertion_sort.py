7d0615c07d7e116840d7b98074abd11cc946f0f5
Yufan Yang <yufan.yang@nagrastar.com>
2024-05-03 16:52:30 -0600

    sorting

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+
+def insertionSort(arr):
+    for i in range(1,len(arr)):
+        j = i-1
+        while j >0
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+for i in range(len(arr)):
+    print ("% d" % arr[i])
