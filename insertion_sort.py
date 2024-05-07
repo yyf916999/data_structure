6b0a1d254fcc4eeda7407da7244360d80fcc9951
Yufan Yang <yufan.yang@nagrastar.com>
2024-05-07 16:54:36 -0600

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,9 +1,18 @@
 
 def insertionSort(arr):
-    for i in range(1,len(arr)):
-        j = i-1
-        while j >0
-arr = [12, 11, 13, 5, 6]
+    for i in range(0,len(arr)-1):
+        print(arr)
+        print(i)
+        j = i
+        val = arr[j+1]
+        while j >= 0 and val < arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = val
+arr = [12, 11, 13, 5, 6,7,33]
 insertionSort(arr)
-for i in range(len(arr)):
-    print ("% d" % arr[i])
+print("________________")
+print(arr)
+
+
+#time complexity: O(n^2)
